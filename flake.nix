634e4f263d650b55006bb9ca3b5049304129bd80
Sergio Tessaris <tessaris@inf.unibz.it>
2023-11-22 13:14:00 +0100

    setup: use flake-parts instead of flake-utils

--- flake.nix
+++ flake.nix
@@ -1,28 +1,35 @@
 {
   description = "Software for AI course labs";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    unstableNixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { self, nixpkgs, unstableNixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-          pkgs = nixpkgs.legacyPackages.${system};
-          uPkgs = unstableNixpkgs.legacyPackages.${system};
-          envName = "ailab";
-          defEnv = pkgs.buildEnv {
-                    name = "${envName}";
-                    paths = [
-                      pkgs.micromamba
-                      pkgs.fast-downward
-                      pkgs.gojq
-                    ];
-          };
-      in
-      {
-        packages.${envName} = defEnv;
+
+  outputs = inputs@{ flake-parts, ... }:
+    # use `flake-parts` for multiple architectures
+    #   see <https://ayats.org/blog/no-flake-utils/>
+    #   and <https://github.com/hercules-ci/flake-parts/blob/main/template/default/flake.nix>
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: let
+        defEnv = pkgs.buildEnv {
+          name = "ailab";
+          paths = with pkgs; [
+            micromamba
+            fast-downward
+            gojq
+            just
+          ];
+        };
+      in {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        packages.${defEnv.name} = defEnv;
         packages.default = defEnv;
-      }
-    );
+      };
+    };
+
 }
