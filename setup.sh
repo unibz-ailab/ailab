93dfd189c1dd6f03a1d2558c92b6e2a35f2b299a
Sergio Tessaris <tessaris@inf.unibz.it>
2023-11-23 13:02:47 +0100

    setup: fixed non POSIX errors in script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -ex
 
 ### Install nix and required packages
 ###   see <https://github.com/DeterminateSystems/nix-installer>
 #
 NIX_INST_VERSION="v0.15.1"
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix/tag/${NIX_INST_VERSION} | sh -s -- install --no-confirm
-source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+curl --proto '=https' --tlsv1.2 -sSf -L "https://install.determinate.systems/nix/tag/${NIX_INST_VERSION}" | sh -s -- install --no-confirm
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 ## Install default profile
 AILAB_FLAKE="github:unibz-ailab/ailab#ailab"
@@ -20,5 +20,5 @@ MINIFORGE_PATH="${HOME}/conda"
 miniforge_installer="Miniforge3-$(uname)-$(uname -m).sh"
 curl -L -o "/tmp/${miniforge_installer}" "https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/${miniforge_installer}"
 sh "/tmp/${miniforge_installer}" -b -p "${MINIFORGE_PATH}"
-"${MINIFORGE_PATH}/bin/mamba" init $(basename "${SHELL:-bash}")
+"${MINIFORGE_PATH}/bin/mamba" init "$(basename "${SHELL:-bash}")"
 "${MINIFORGE_PATH}/bin/conda" config --set auto_activate_base false
