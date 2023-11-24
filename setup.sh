ab7e78d57c22bb471736a0ed7d444c47ff973409
Sergio Tessaris <tessaris@inf.unibz.it>
2023-11-24 17:21:05 +0100

    setup: refactored install script

--- setup.sh
+++ setup.sh
@@ -1,24 +1,44 @@
 #!/usr/bin/env sh
 set -ex
 
-### Install nix and required packages
-###   see <https://github.com/DeterminateSystems/nix-installer>
-#
-NIX_INST_VERSION="v0.15.1"
-curl --proto '=https' --tlsv1.2 -sSf -L "https://install.determinate.systems/nix/tag/${NIX_INST_VERSION}" | sh -s -- install --no-confirm
-. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+setup_nix() {
+    ### Install nix and required packages
+    ###   see <https://github.com/DeterminateSystems/nix-installer>
+    #
+    NIX_INST_VERSION="v0.15.1"
+    curl --proto '=https' --tlsv1.2 -sSf -L "https://install.determinate.systems/nix/tag/${NIX_INST_VERSION}" | sh -s -- install --no-confirm
+    . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+}
 
-## Install default profile
-AILAB_FLAKE="github:unibz-ailab/ailab#ailab"
-nix profile install "${AILAB_FLAKE}"
+setup_miniforge() {
+    ### Install Miniforge
+    ###   see <https://github.com/conda-forge/miniforge#downloading-the-installer-as-part-of-a-ci-pipeline>
+    #
+    MINIFORGE_VERSION=23.3.1-1
+    MINIFORGE_PATH="${HOME}/conda"
 
-### Install Miniforge
-###   see <https://github.com/conda-forge/miniforge#downloading-the-installer-as-part-of-a-ci-pipeline>
-#
-MINIFORGE_VERSION=23.3.1-1
-MINIFORGE_PATH="${HOME}/conda"
-miniforge_installer="Miniforge3-$(uname)-$(uname -m).sh"
-curl -L -o "/tmp/${miniforge_installer}" "https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/${miniforge_installer}"
-sh "/tmp/${miniforge_installer}" -b -p "${MINIFORGE_PATH}"
-"${MINIFORGE_PATH}/bin/mamba" init "$(basename "${SHELL:-bash}")"
-"${MINIFORGE_PATH}/bin/conda" config --set auto_activate_base false
+    temp_dir="$(mktemp -d)" || return 1
+    script_name="Miniforge3-$(uname)-$(uname -m).sh"
+    script_file="${temp_dir}/${script_name}"
+
+    curl -L -o "${script_file}" "https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VERSION}/${script_name}"
+    sh -- "${script_file}" -b -p "${MINIFORGE_PATH}"
+    rm -rf "${temp_dir}"
+
+    "${MINIFORGE_PATH}/bin/mamba" init "$(basename "${SHELL:-bash}")"
+    "${MINIFORGE_PATH}/bin/conda" config --set auto_activate_base false
+
+    . "${MINIFORGE_PATH}/etc/profile.d/conda.sh"
+    . "${MINIFORGE_PATH}/etc/profile.d/mamba.sh"
+}
+
+main() {
+    setup_nix
+    setup_miniforge
+
+    ## Configure default Nix profile
+    AILAB_FLAKE="github:unibz-ailab/ailab#ailab"
+    nix profile install "${AILAB_FLAKE}"
+}
+
+main
